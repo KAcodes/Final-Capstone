d12260926a2b11484bc882e2a619f1d97cecd7e4
Kayode Apena <kayodeapena@gmail.com>
2023-01-11 15:23:55 +0000

    made save for later func work

--- Capstone Project/functionalityGrace.js
+++ Capstone Project/functionalityGrace.js
@@ -1,44 +1,46 @@
 let savedContent = {};
 
-const saveContent = (key, value, event) => {
+const saveContent = (key, event) => {
     //console.log(event.currentTarget)
     event.currentTarget.classList.toggle("saved");
 
      if (event.currentTarget.classList.contains("saved")) {
         
-        let element = (event.currentTarget.closest(".section"));
-        console.log(element)
+        //let element = (event.currentTarget.closest(".section"));
+        let element = (event.currentTarget.closest(".section"))
+        let words = element.innerHTML
+
         savedContent = {
             ...savedContent,
-            [key]: value
+            [key]: words
           };
         sessionStorage.setItem('savedContent', JSON.stringify(savedContent));
-        //console.log()
         console.log(sessionStorage.getItem('savedContent'));
 
     }   else {
         delete savedContent[key];
         sessionStorage.setItem('savedContent', JSON.stringify(savedContent));
-        //console.log(sessionStorage.getItem('savedContent'));  
+        console.log(sessionStorage.getItem('savedContent'));  
     } 
 
-    /* const showSaved = () => {
-        for (const key in savedContent) {
-            savedSpace.append(savedContent[key]);
-        }
-    }; */
-
-
+  
 };
 
-const deleteItem = (key) => {
-    
-}
+
 
 const savedSpace = document.getElementById('savedSpace');
 
+
+
 const showSaved = () => {
-    for (const key in savedContent) {
-        savedSpace.append(savedContent[key]);
-    }
-};
+   
+    
+    let savedCon = JSON.parse(sessionStorage.getItem('savedContent'))
+    
+    for (const key in savedCon) {
+        let divy = document.createElement('div');
+        divy.innerHTML = (savedCon[key]);
+        savedSpace.append(divy);
+        
+    } 
+}; 
